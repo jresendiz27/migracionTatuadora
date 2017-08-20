cf5a21d728a75e5327b9d090e9da6c06635be43d
Juan Resendiz <jresendiz27@gmail.com>
2017-08-20 14:08:36 -0500

    Adding truncate table clauses to DatabaseTool
    Logging process is now part of a method, firsRow is logged now

--- src/main/groovy/mx/blick/migracion/DatabaseTool.groovy
+++ src/main/groovy/mx/blick/migracion/DatabaseTool.groovy
@@ -7,7 +7,7 @@ import java.sql.SQLException
 
 @Singleton(strict = false)
 @Log4j
-public class DatabaseTool {
+class DatabaseTool {
 
     private String url = "jdbc:mysql://localhost:3306/test";
     private String user = "root";
@@ -20,13 +20,43 @@ public class DatabaseTool {
         sql = Sql.newInstance(url, user, password, driver);
     }
 
+    void truncateUserRelatedTables() {
+        this.truncateUser()
+        this.truncateStudio()
+        this.truncateFreelance()
+    }
+
+    void truncateUser() {
+        String truncateUserQuery = "TRUNCATE TABLE latatuadora_core.User"
+        log.error(truncateUserQuery)
+        sql.execute(truncateUserQuery);
+    }
+
+    void truncateStudio() {
+        String truncateStudioQuery = "TRUNCATE TABLE latatuadora_core.Studio"
+        log.error(truncateStudioQuery);
+        sql.execute(truncateStudioQuery);
+    }
+
+    void truncateFreelance() {
+        String truncateFreelance = "TRUNCATE TABLE latatuadora_core.Freelance"
+        log.error(truncateFreelance)
+        sql.execute(truncateFreelance);
+    }
+
+    void truncateStudioStyles() {
+        String truncateStudioStyle = "TRUNCATE TABLE latatuadora_core.StudioStyle"
+        log.error(truncateStudioStyle);
+        sql.execute(truncateStudioStyle);
+    }
+
     void insertUser(Map values) {
         String insertUserSQL = """
                     INSERT INTO 
                         latatuadora_core.User(name, lastname, email, password, telephone, userType, createdAt, updatedAt) 
                     values 
                         (:name, :lastname, :email, '$defaultPassword', :telephone, :userType, NOW(), NOW())"""
-        sql.execute(insertUserSQL, values)
+        this.executeSimpleQuery(insertUserSQL, values)
     }
 
     void insertStudio(Integer userId, Map values) {
@@ -37,7 +67,7 @@ public class DatabaseTool {
                         (:name, :certCofepris, :addressId, TRUE, :titleImgUrl, :logoUrl, :profileImgUrl, :about, :userId, ${
             Constants.STUDIO_STATUS.publicate
         }, NOW(), NOW())"""
-        sql.execute(insertUserSQL, values)
+        this.executeSimpleQuery(insertUserSQL, values)
     }
 
     void insertFreelance(Integer userId, Map values) {
@@ -46,7 +76,7 @@ public class DatabaseTool {
                         latatuadora_core.Freelancer(user, about, published, profileImgUrl, canGoHome, name, rank, createdAt, updatedAt)
                     values 
                         ($userId, :about, TRUE, :profileImgUrl, :canGoHome, :name, 5, NOW(), NOW())"""
-        sql.execute(insertUserSQL, values)
+        this.executeSimpleQuery(insertUserSQL, values)
     }
 
     Map findByUserIdOrEmail(Integer id, String email) {
@@ -58,9 +88,57 @@ public class DatabaseTool {
                 WHERE
                     id = $id
                 OR
-                    email like ('$email')
+                    TRIM(LOWER(email)) like ('%${email.toLowerCase().trim()}%')
             """
-        return sql.firstRow(findUserByIdOrEmailQuery) as Map;
+        this.queryFirstRow(findUserByIdOrEmailQuery)
     }
 
+    Map findStudioByIdOrEmail(Integer id, String email) {
+        String findStudioByIdOrEmailQuery = """
+                SELECT 
+                    Studio.id, Studio.name, User.email
+                FROM
+                    latatuadora_core.Studio 
+                INNER JOIN
+                    latatuadora_core.User in Studio.userId = User.id
+                WHERE
+                    User.id = $id
+                OR
+                    TRIM(LOWER(email)) like ('%${email.toLowerCase().trim()})%')
+            """
+    }
+
+    Map findStyleByName(String styleName) {
+        String styleByName = """
+            SELECT 
+                id, name 
+            FROM 
+                latatuadora_core.Style 
+            WHERE 
+                TRIM(LOWER(name)) like  (%${styleName.toLowerCase().trim()}%)"""
+        return this.queryFirstRow(styleByName)
+    }
+
+    void executeSimpleQuery(String query, Map params) {
+        this.logQuery(query, params)
+        sql.execute(query, params)
+    }
+
+    Map queryFirstRow(String query, Map params = null) {
+        this.logQuery(query, params)
+        return sql.firstRow(query) as Map;
+    }
+
+    void logQuery(String query, Map params) {
+        String sanitizedQuery = ""
+        String auxQuery = "${query}"
+
+        for (key in params.keySet()) {
+            String namedParam = ":${key}"
+            if (auxQuery.indexOf(namedParam)) {
+                auxQuery = auxQuery.replace(namedParam, params.get(key) as String)
+            }
+        }
+        log.error(sanitizedQuery)
+    }
 }
