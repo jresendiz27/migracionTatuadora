01b8f09673432093346a84183e93edb55612cfe5
Juan Resendiz <jresendiz27@gmail.com>
2017-08-20 21:06:19 -0500

    Adding TODOs to work with the images association

--- src/main/groovy/mx/blick/migracion/MigrateStudiosAndFreelancers.groovy
+++ src/main/groovy/mx/blick/migracion/MigrateStudiosAndFreelancers.groovy
@@ -13,11 +13,13 @@ class MigrateStudiosAndFreelancers {
         List<Map> studios = csvReader.studios
         List<Map> freelancers = csvReader.freelancers
 
-        databaseTool.truncateUser();
-        databaseTool.truncateStudio();
+        databaseTool.truncateUser()
+        databaseTool.truncateStudio()
         databaseTool.truncateFreelance()
         databaseTool.truncateAddress()
-
+        // TODO associate artists with studios
+        // TODO associate tattoos images with studios and freelancers, upload them to the server?
+        // TODO create an inmemory map to know the user/studio/freelancer id based on the email
         studios.each { studioMap ->
             Long createdAddressId = databaseTool.createAddress(studioMap).first()[0] as Long
             Long createdUserId = databaseTool.insertUserStudio(studioMap).first()[0] as Long
